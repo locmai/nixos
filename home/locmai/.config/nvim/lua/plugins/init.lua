83e5de20caa976876b2e8072d01c6d1187e8833e
locmai <locmai0201@gmail.com>
2024-10-13 22:27:59 +0700

    fix: remove format on save

--- home/locmai/.config/nvim/lua/plugins/init.lua
+++ home/locmai/.config/nvim/lua/plugins/init.lua
@@ -23,7 +23,7 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    -- event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 

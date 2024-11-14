98dbc5810adea061aa16555c9d4c1f3bbd733d84
locmai <locmai0201@gmail.com>
2024-11-14 12:36:19 +0700

    chore: add trivy

--- nixos/kubernetes.nix
+++ nixos/kubernetes.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     k9s
     kubernetes-helm
     kubectl
     kubectx
     istioctl
-    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    operator-sdk
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
   ];
 }

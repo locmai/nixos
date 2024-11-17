2cb460f70856e98390e1b687b5ba018e957c18b2
locmai <locmai0201@gmail.com>
2024-11-17 19:44:09 +0700

    chore: add kind

--- nixos/kubernetes.nix
+++ nixos/kubernetes.nix
@@ -1,11 +1,15 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     k9s
+    kind
     kubernetes-helm
     kubectl
     kubectx
     istioctl
     operator-sdk
+
+    terragrunt
+    opentofu
     (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
   ];
 }

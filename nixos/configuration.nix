36b2879e62487a29eba7ee12626f1c1dd7f3df7b
locmai <locmai0201@gmail.com>
2025-05-04 18:24:35 +0700

    performancemake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,7 +139,7 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "performance";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     };

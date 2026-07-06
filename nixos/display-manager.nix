78fdbca6843d0905c2f12f3caab2b371669726cd
locmai <locmai0201@gmail.com>
2026-07-06 12:33:39 -0700

    update start-hyprland command

--- nixos/display-manager.nix
+++ nixos/display-manager.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --time-format '%I %M %p | %a * %h | %F' --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --time-format '%I %M %p | %a * %h | %F' --cmd start-hyprland";
         user = "greeter";
       };
     };
